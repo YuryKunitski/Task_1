69399a16aabcdbd66d800895d0d8bde022150968
YuryKunitski <yura.kunickij@gmail.com>
2019-03-11 16:49:12 +0300

    commit in branch master

---
+++ src/by/epam/javawebtraining/kunitski/task01/view/LogPrinter.java
@@ -0,0 +1,12 @@
+package by.epam.javawebtraining.kunitski.task01.view;
+
+/**
+ * I made this class for my printer's hierarchy and I don't know how do it exactly.
+ */
+
+public class LogPrinter implements Printable {
+  @Override
+  public void print(Object massage) {
+
+  }
+}
